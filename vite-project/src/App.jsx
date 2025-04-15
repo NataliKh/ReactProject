e9e3f70ae692884625ba8363a4d9100ba44280fd
Natali Khudyakova <natali272014@gmail.com>
2025-04-15 12:59:06 +0400

    Второе задание по компонентам Рецепт

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,87 +1,84 @@
-import { useState } from "react";
-import styles from "./app.module.css";
+import styles from './app.module.css';
+import data from './data.json';
+import { useState } from 'react';
 
-function App() {
-  const [value, setValue] = useState("");
-  const [error, setError] = useState("");
-  const [list, setList] = useState([]);
-  const [isValueValid, setIsValueValid] = useState(false);
+export default function App() {
+  const [steps, setSteps] = useState(data); // Изначально пустой массив
+  const [activeIndex, setActiveIndex] = useState(0);
 
-  const onAddButtonClick = () => {
-    if (isValueValid) {
-      const id = Date.now();
-      const updatedList = [...list, { id, value }];
-      setList(updatedList);
-      setValue("");
-      setError("");
-      setIsValueValid(false);
-    }
-  };
 
-  const onInputButtonClick = () => {
-    const promptValue = prompt("Введите значение", "");
-    if (promptValue.length < 3) {
-      setError("Введенное значение должно содержать минимум 3 символа");
-      setIsValueValid(false);
+
+  const isFirstStep = activeIndex === 0;
+  const isLastStep = activeIndex === steps.length - 1;
+
+  const onClickHandlerPrev = () => {
+    if (!isFirstStep) {
+      setActiveIndex(activeIndex - 1);
     } else {
-      setIsValueValid(true);
-      setError("");
-      setValue(promptValue);
+      setSteps([...steps]);
+      setActiveIndex(steps.length - 1);
     }
-    return promptValue;
   };
 
-  const getDateString = (date) => {
-    date = new Date(date);
-    
-    const day = String(date.getDate()).padStart(2, '0');
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const year = date.getFullYear();
-    
-    const hours = String(date.getHours()).padStart(2, '0');
-    const minutes = String(date.getMinutes()).padStart(2, '0');
-    const seconds = String(date.getSeconds()).padStart(2, '0');
-    
-    return `${day}.${month}.${year} ${hours}:${minutes}:${seconds}`;
+  const onClickHandlerNext = () => {
+    if (!isLastStep) {
+      setActiveIndex(activeIndex + 1);
+    } else {
+      setSteps([...steps]);
+      setActiveIndex(0);
+    }
   };
+
+  const onClickHandlerDone = (e) => {
+    const index = e.target.textContent;
+    setSteps([...steps]);
+    setActiveIndex(index - 1);
+  };
+
+  if (!steps[activeIndex]) {
+    return <div>Нет данных</div>;
+  }
 
   return (
-    <>
-      <h1 className={styles["page-heading"]}>Ввод значения</h1>
-      <p className={styles["no-margin-text"]}>
-        Текущее значение <code> value </code>: "
-        <output className={styles["current-value"]}>{value}</output>"
-      </p>
-      {error && <div className={styles["error"]}>{error}</div>}
-      <div className={styles["buttons-container"]}>
-        <button className={styles["button"]} onClick={onInputButtonClick}>
-          Ввести новое
-        </button>
-        <button
-          className={styles["button"]}
-          disabled={!isValueValid}
-          onClick={onAddButtonClick}
-        >
-          Добавить в список
-        </button>
-      </div>
-      <div className={styles["list-container"]}>
-        <h2 className={styles["list-heading"]}>Список:</h2>
-        {list.length <= 0 && (
-          <p className={styles["no-margin-text"]}>Нет добавленных элементов</p>
-        )}
-        {list.length > 0 && (
-          <ul className={styles["list"]}>
-            {list.map(({ id, value }) => (
-              <li key={id} className={styles["list-item"]}>
-                {'[' + getDateString(id) + '] ' + value}
+    <div className={styles.container}>
+      <div className={styles.card}>
+        <h1>{steps[activeIndex]?.title || "Нет данных"}</h1>
+        <div className={styles.steps}>
+          <div className={styles['steps-content']}>
+            <div className={styles['steps-content-text']}>
+              {steps[activeIndex]?.content || "Контент отсутствует"}
+            </div>
+          </div>
+          <ul className={styles['steps-list']}>
+            {steps.map((step, index) => (
+              <li
+                className={`${styles['steps-item']} ${
+                  activeIndex === index ? styles.active : ''
+                } ${index < activeIndex ? styles.done : ''}`}
+                key={step.id}
+              >
+                  <button className={styles['steps-item-button']} onClick={onClickHandlerDone}>{index + 1}</button>
+                  {step.title}
               </li>
             ))}
           </ul>
-        )}
+          <div className={styles['buttons-container']}>
+            <button
+              className={styles.button}
+              onClick={onClickHandlerPrev}
+              disabled={isFirstStep && activeIndex === 0}
+            >
+              Назад
+            </button>
+            <button
+              className={styles.button}
+              onClick={onClickHandlerNext}
+            >
+              {isLastStep ? 'Начать сначала' : 'Далее'}
+            </button>
+          </div>
+        </div>
       </div>
-    </>
+    </div>
   );
-}
-
-export default App;
+};
