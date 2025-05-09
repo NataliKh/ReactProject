2086ccfbaec4bc4f456801223ab2caf95c760576
Natali Khudyakova <natali272014@gmail.com>
2025-05-09 14:10:49 +0400

    react-hook-form

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,38 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import styles from "./App.module.css";
+
+const schema = yup.object({
+  email: yup
+    .string()
+    .email("Некорректный email")
+    .required("Заполните email"),
+  password: yup
+    .string()
+    .min(3, "Пароль должен содержать минимум 3 символа")
+    .max(20, "Пароль должен содержать максимум 20 символов")
+    .required("Введите пароль"),
+  passwordRepeat: yup
+    .string()
+    .oneOf([yup.ref("password")], "Пароли не совпадают")
+    .required("Повторите пароль"),
+});
 
 function App() {
-  const [count, setCount] = useState(0)
-  const date = new Date().getFullYear();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm({
+    resolver: yupResolver(schema),
+    mode: "onChange",
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => //императивный код, остальное декларативный
-            setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      {date}
-    </>
-  )
+    <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+      <input className={styles.formControl}
+        {...register("email")}
+        type="email"
+        placeholder="Введите email"
+      />
+      {errors.email && (
+        <div className={styles.validationError}>{errors.email.message}</div>
+      )}
+
+      <input className={styles.formControl}
+        {...register("password")}
+        type="password"
+        placeholder="Введите пароль"
+      />
+      {errors.password && (
+        <div className={styles.validationError}>{errors.password.message}</div>
+      )}
+
+      <input className={styles.formControl}
+        {...register("passwordRepeat")}
+        type="password"
+        placeholder="Повторите пароль"
+      />
+      {errors.passwordRepeat && (
+        <div className={styles.validationError}>
+          {errors.passwordRepeat.message}
+        </div>
+      )}
+
+      <button type="submit" disabled={!isValid}>
+        Зарегистрироваться
+      </button>
+    </form>
+  );
 }
 
-export default App
+export default App;
