f2a61492d103582dfd477dacbcbc79fecef99e38
Natali Khudyakova <natali272014@gmail.com>
2025-07-07 13:16:16 +0400

    Рабочий вариант

--- vite-project/src/components/information/ImformationLayout.jsx
+++ vite-project/src/components/information/ImformationLayout.jsx
@@ -1,12 +1,7 @@
-import PropTypes from "prop-types";
-export const InformationLayout = ({children}) => {
+export const InformationLayout = ({ children }) => {
   return (
     <div>
       <h1>{children}</h1>
     </div>
   );
 };
-
-InformationLayout.propTypes = {
-  children: PropTypes.string.isRequired
-}
